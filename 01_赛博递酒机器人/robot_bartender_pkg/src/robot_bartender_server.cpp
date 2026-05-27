b074a32a9dfe2230d5e740b39108539b801e7f6f
liuxc666 <liuxuechengyyds@gmail.com>
2026-05-27 09:17:21 +0800

    260527更新

--- 01_赛博递酒机器人/robot_bartender_pkg/src/robot_bartender_server.cpp
+++ 01_赛博递酒机器人/robot_bartender_pkg/src/robot_bartender_server.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <thread>
 #include <queue>
+#include <cmath> // 引入 std::sqrt 和 std::abs
 
 #include "rclcpp/rclcpp.hpp"
 #include "robot_interface/action/robot_msg.hpp"
@@ -60,22 +61,16 @@ private:
 
     void start_delivering_alcohol(const std::shared_ptr<GoalHandleRobotMsg> goal_handle){
         RCLCPP_INFO(this->get_logger(), "送酒任务开始");
-        
-        auto goal_data = goal_queue_.front();
 
-        //goal信息
-        int target_x = goal_data->target_x;
-        int target_y = goal_data->target_y;
-        std::string item_name = goal_data->item_name;
-
-        //feedback信息
+        //实例化feedback信息
         std::string current_mode;
-        int current_coordinates;
-        int remaining_power;
+        int current_x = 0;
+        int current_y = 0;
+        int remaining_power = 100;
 
-        //result信息
-        bool is_finish;
-        int total_duration;
+        //实例化result信息
+        bool is_finish = false;
+        int total_duration = 0;
 
         //使用枚举控制状态机状态
         enum RobotMode{
@@ -87,6 +82,39 @@ private:
 
         RobotMode robot_status = Idle;
 
+        //队列不是空，则循环
+        while(! goal_queue_.empty()){
+
+            if (goal_handle->is_canceling()) {
+                RCLCPP_INFO(this->get_logger(), "收到指令，任务取消，回到充电桩");
+                result->is_finish = false;
+                result->total_duration = total_duration;
+                goal_handle->canceled(result);
+                return; 
+            }
+
+            auto goal_data = goal_queue_.front();
+            //将队列首位塞入变量
+            //提取goal信息
+            int target_x = goal_data->target_x;
+            int target_y = goal_data->target_y;
+            std::string item_name = goal_data->item_name;
+            
+            //判断当前状态
+            RobotMode robot_status = (remaining_power <= 20) ? LowBattery : Delivering;
+            target_x = (RobotMode robot_status = Delivering) ? target_x : 0;
+
+        }
+
+        //成功完成任务
+        if(result->is_finish){
+            goal_handle->succeed(result);
+            RCLCPP_INFO(this->get_logger(), "成功完成任务，总用时：%i 分钟",total_duration);
+        }else{
+            goal_handle->abort(result);
+            RCLCPP_INFO(this->get_logger(), "任务成功取消，总用时：%i 分钟",total_duration);
+        }
+    	
 
     }
 
